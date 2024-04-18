9a56c3db269d3484bf7f518bc52d01e761006de3
YunjiShin <nyunjisi@gmail.com>
2024-04-18 15:47:30 +0900

    urls&templates setting finish

--- RestaurantShare/shareRes/urls.py
+++ RestaurantShare/shareRes/urls.py
@@ -14,11 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
-    path('', include('shareRes.urls')),
-    path('sendEmail/', include('sendEmail.urls')),
-    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('restaurantDetail/', views.restaurantDetail),
+    path('restaurantCreate/', views.restaurantCreate),
+    path('categoryCreate/', views.categoryCreate),
+
 ]
--- RestaurantShare/shareRes/views.py
+++ RestaurantShare/shareRes/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    # return HttpResponse("index")
+    return render(request, 'shareRes/index.html')
+def restaurantDetail(request):
+    # return HttpResponse("restaurantDetail")
+    return render(request, 'shareRes/restaurantDetail.html')
+
+def restaurantCreate(requset):
+    # return HttpResponse("restaurantCreate")
+    return render(requset, 'shareRes/restaurantCreate.html')
+
+def categoryCreate(requset):
+    # return HttpResponse("categoryCreate")
+    return render(requset, 'shareRes/categoryCreate.html')
